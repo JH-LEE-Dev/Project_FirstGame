64b3e5dedcadba612ca3ccb71404dc6bf579e03e
messier3331 <messier3331@naver.com>
2026-01-06 18:59:17 +0900

    Update CardData.cs

--- Assets/Scripts/GameplaySystem/CardSystem/Card/CardData.cs
+++ Assets/Scripts/GameplaySystem/CardSystem/Card/CardData.cs
@@ -6,8 +6,14 @@ using UnityEngine.Audio;
 public class CardData
 {
     public int id;
-    public int cost;
+    public int cost; // 지우셈
 
-    public CardType type;
+    public CardType cardType;
+    public ElementType elementType;
+    public UsingType usingType;
+
+    public bool bUpgrade = false;
+    public bool bFlash = false; // 한 턴 지나면 사라질 놈 (인게임중에 다른 카드 효과에 의해 만들어진놈)
+
     public List<CardEffectData> effects;
 }
