9fae9ac72aa99d532e95b7c1ac22c2d9c1423319
VOID <void91112@gmail.com>
2026-01-13 16:28:55 +0900

    🔶 TextMotion Script 모션 기능 추가 중

--- Assets/Scripts/UI/TextUI/TextMotion.cs
+++ Assets/Scripts/UI/TextUI/TextMotion.cs
@@ -1,8 +1,10 @@
 using DG.Tweening;
+using NaughtyAttributes;
 using System;
 using TMPro;
 using UnityEngine;
-using static UnityEngine.Rendering.DebugUI;
+
+using Sequence = DG.Tweening.Sequence;
 
 public class TextMotion : MonoBehaviour
 {
@@ -10,8 +12,31 @@ public class TextMotion : MonoBehaviour
     [SerializeField] private TextMeshProUGUI mainText;
     [SerializeField] private float motionDuration = 1f;
     [SerializeField] private Ease motionEase = Ease.Linear;
+    [SerializeField] private bool bColorChange = false;
+    [SerializeField] private bool bShaking = false;
 
-    public void OnHit(float _prev, float _current)
+    [Header("Color Change Settings")]
+    [ShowIf("bColorChange"), SerializeField] private Color defaultColor = Color.white;
+    [ShowIf("bColorChange"), SerializeField] private Color callColor = Color.softRed;
+
+    [Header("Shaking Settings")]
+    [ShowIf("bShaking"), SerializeField] private RectTransform visualRect = null;
+    [ShowIf("bShaking"), SerializeField] private float shakeDuration = 1f;
+    [ShowIf("bShaking"), SerializeField] private float shakePower = 5f;
+    [ShowIf("bShaking"), SerializeField] private Ease shakeEase = Ease.Linear;
+
+    private Sequence colorSeq = null;
+    private Sequence shakeSeq = null;
+
+    private Vector2 originalAnchoredPos = Vector2.zero;
+
+    private void Awake()
+    {
+        if(null != visualRect)
+            originalAnchoredPos = visualRect.anchoredPosition;
+    }
+
+    public void OnHit(float _prev, float _current, float _motionDuration = -1f)
     {
         if (null == mainText)
             return;
@@ -20,6 +45,9 @@ public class TextMotion : MonoBehaviour
         {
             mainText.text = Mathf.RoundToInt(value).ToString();
         }).SetEase(motionEase).SetUpdate(false);
+
+        OnColorChange();
+        OnShake(_motionDuration);
     }
     
     
@@ -31,5 +59,52 @@ public class TextMotion : MonoBehaviour
         float convertedValue = Convert.ToSingle(_value);
         Debug.Log(convertedValue);
         mainText.text = Mathf.RoundToInt(convertedValue).ToString();
+    }
+
+    private void OnColorChange()
+    {
+        if (!bColorChange)
+            return;
+
+        colorSeq = CancelPrevMotion(colorSeq);
+
+        colorSeq.AppendCallback(() =>
+        {
+            mainText.color = callColor;
+        });
+
+        colorSeq.Append(mainText.DOColor(defaultColor, motionDuration)
+            .SetEase(motionEase)
+            .SetUpdate(false));
+    }
+
+    private void OnShake(float _motionDuration)
+    {
+        if (!bShaking || null == visualRect)
+            return;
+
+        float finalDuration = _motionDuration < 0f ? shakeDuration : _motionDuration;
+
+        shakeSeq = CancelPrevMotion(shakeSeq);
+
+        shakeSeq.AppendCallback(() =>
+        {
+            visualRect.anchoredPosition = originalAnchoredPos;
+        });
+
+        shakeSeq.Append(visualRect.DOShakeAnchorPos(finalDuration, shakePower)
+            .SetEase(shakeEase)
+            .SetUpdate(false));
+    }
+
+    private Sequence CancelPrevMotion(Sequence target)
+    {
+        if (null == target)
+            return null;
+
+        if (target.IsActive())
+            target.Kill();
+
+        return DOTween.Sequence();
     }
 }
