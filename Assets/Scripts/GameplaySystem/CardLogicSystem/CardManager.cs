48004d3913b81d2e220eccd7ba3864439d2f1090
JH-LEE-Dev <140565804+JH-LEE-Dev@users.noreply.github.com>
2026-01-13 10:33:09 +0900

    카드 드로우 종류 수정.

--- Assets/Scripts/GameplaySystem/CardLogicSystem/CardManager.cs
+++ Assets/Scripts/GameplaySystem/CardLogicSystem/CardManager.cs
@@ -89,7 +89,7 @@ public class CardManager : MonoBehaviour, ICardSystemProvider, ICardEffectComman
 
     public void Start()
     {
-        CardData cardData = cardDataBase.GetCardData(1);
+        CardData cardData = cardDataBase.GetCardData(0);
         if (cardData == null)
             return;
 
