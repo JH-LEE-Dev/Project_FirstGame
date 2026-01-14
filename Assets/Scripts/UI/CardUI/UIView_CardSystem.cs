6b422ceeb2cb1095b0d65fa8728efef5467f579d
VOID <void91112@gmail.com>
2026-01-14 18:27:14 +0900

    🔶 Test Impact 이펙트 제거

--- Assets/Scripts/UI/CardUI/UIView_CardSystem.cs
+++ Assets/Scripts/UI/CardUI/UIView_CardSystem.cs
@@ -61,10 +61,6 @@ public class UIView_CardSystem : UIView
     private bool bWorkingBlock = false;
     public bool WorkingBlock { get { return bWorkingBlock; } set { bWorkingBlock = value; } }
 
-    public MeshRenderer testImpact = null;
-    private Material mat = null;
-    private ParticleSystem particle = null;
-
     public override void OnDestroy()
     {
         UICommandCompleteEvent = null;
@@ -93,31 +89,6 @@ public class UIView_CardSystem : UIView
         graveSystem?.Init(this);
         extinctionSystem?.Init(this);
         clickCatchSystem?.Init(this);
-
-        Test();
-    }
-
-    public void Test()
-    {
-        mat = testImpact?.material;
-        particle = testImpact?.gameObject.GetComponentInChildren<ParticleSystem>();
-
-        DG.Tweening.Sequence seq = DOTween.Sequence();
-
-        seq.AppendCallback(() =>
-        {
-            if (particle != null)
-            {
-                particle.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
-                particle.Play();
-            }
-            mat.SetFloat("_Ratio", 0f);
-        });
-
-        seq.Append(mat.DOFloat(1f, "_Ratio", 2f)
-            .SetEase(Ease.OutQuad));
-
-        seq.SetLoops(-1);
     }
 
     // For PoolingSystem
