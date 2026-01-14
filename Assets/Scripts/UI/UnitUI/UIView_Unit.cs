8f30691a3f840a911af203aeddcf353d8c196811
JH-LEE-Dev <140565804+JH-LEE-Dev@users.noreply.github.com>
2026-01-14 15:51:28 +0900

    UIView_Unit에 PlayerData 주입.

--- Assets/Scripts/UI/UnitUI/UIView_Unit.cs
+++ Assets/Scripts/UI/UnitUI/UIView_Unit.cs
@@ -4,11 +4,17 @@ public class UIView_Unit : UIView
 {
     [Header("UI References")]
     [SerializeField] private Transform uiRoot;
- 
+
+    IPlayerData playerData;
+
     protected override void Awake()
     {
         base.Awake();
+    }
 
+    public void DataInjection(IPlayerData _playerData)
+    {
+        playerData = _playerData;
     }
 
     public override void Update()
