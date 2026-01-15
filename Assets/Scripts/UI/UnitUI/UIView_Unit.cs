692149158e3ba5db85eb1ae9f0a32fcb902ce9b6
JH-LEE-Dev <140565804+JH-LEE-Dev@users.noreply.github.com>
2026-01-15 10:50:28 +0900

    BulletSocket 코드 null체크 추가

--- Assets/Scripts/UI/UnitUI/UIView_Unit.cs
+++ Assets/Scripts/UI/UnitUI/UIView_Unit.cs
@@ -24,6 +24,7 @@ public class UIView_Unit : UIView
 
     public void Initialize(ICharacterData _characterData)
     {
+        Debug.Log("1");
         characterData = _characterData;
         // 임시로 시작은 2개 연출중. 나중에 매개변수로 캐릭터 타입 넣어주면 될듯.
         SetBulletSocket();
