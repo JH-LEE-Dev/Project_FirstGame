0df7a288263ae28858991168ea9d32310431a12f
messier3331 <messier3331@naver.com>
2026-01-19 19:08:15 +0900

    개빡치는 피킹작업
    ㅇㅇ

--- Assets/Scripts/UI/CardUI/ClickCatchSystem.cs
+++ Assets/Scripts/UI/CardUI/ClickCatchSystem.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-public class ClickCatchSystem : MonoBehaviour
+public class ClickCatchSystem : MonoBehaviour, IPointerClickHandler
 {
     private UIView_Unit uIView_Unit;
+    private WorldCanvasEnabler canvasEnabler;
 
     public void Init(UIView_Unit _uIView_Unit)
     {
         uIView_Unit = _uIView_Unit;
+        canvasEnabler = GetComponent<WorldCanvasEnabler>();
+        canvasEnabler.Initialize();
     }
 
-
-
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left ||
+            eventData.button == PointerEventData.InputButton.Right)
+            uIView_Unit?.CancelPreview();
+    }
 }
