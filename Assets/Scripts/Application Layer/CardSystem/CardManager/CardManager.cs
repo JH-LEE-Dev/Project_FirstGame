8e51ba8ae07933b2f7dac57a597c579e5fa3da9f
JH-LEE-Dev <140565804+JH-LEE-Dev@users.noreply.github.com>
2026-01-21 11:24:30 +0900

    카드 드로우 버그 수정

--- Assets/Scripts/Application Layer/CardSystem/CardManager/CardManager.cs
+++ Assets/Scripts/Application Layer/CardSystem/CardManager/CardManager.cs
@@ -80,7 +80,7 @@ public class CardManager : MonoBehaviour, ICardSystemActionCommandHandler, ICard
 
     public void Start()
     {
-        CardData cardData = cardDataBase.GetCardData(3);
+        CardData cardData = cardDataBase.GetCardData(0);
         if (cardData == null)
             return;
 
