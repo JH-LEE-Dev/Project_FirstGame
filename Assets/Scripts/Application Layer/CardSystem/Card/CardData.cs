22e34f972d48e8bc8ae360dbd19c047126cb3fcc
VOID <void91112@gmail.com>
2026-01-22 15:44:44 +0900

    🔶 카드 데이터 변수 추가

--- Assets/Scripts/Application Layer/CardSystem/Card/CardData.cs
+++ Assets/Scripts/Application Layer/CardSystem/Card/CardData.cs
@@ -12,6 +12,7 @@ public class CardData
     public CardType cardType;
     public ElementType elementType;
     public UsingType usingType;
+    [Multiline(3)] public string cardDescription;
 
     public bool bFlash = false; // 한 턴 지나면 사라질 놈 (인게임중에 다른 카드 효과에 의해 만들어진놈)
 
