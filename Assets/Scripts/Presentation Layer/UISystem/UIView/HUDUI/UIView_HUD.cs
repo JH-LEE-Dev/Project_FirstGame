ee3c4b59aebe7e62096a5a9d141dd68ecbe9cbaa
VOID <void91112@gmail.com>
2026-01-29 18:33:54 +0900

    🔶 디버그 주석

--- Assets/Scripts/Presentation Layer/UISystem/UIView/HUDUI/UIView_HUD.cs
+++ Assets/Scripts/Presentation Layer/UISystem/UIView/HUDUI/UIView_HUD.cs
@@ -231,7 +231,7 @@ public class UIView_HUD : UIView
             return;
 
         targetBar.OnFill(vfx.savedCurrentProgress);
-        Debug.Log(vfx.savedCurrentProgress);
+        //Debug.Log(vfx.savedCurrentProgress);
 
         targetGageText?.DataUpdate(vfx.savedCurrentKillCnt, vfx.savedEnemyMaxCnt);
         StartCoroutine(ReleaseEffect(vfx));
