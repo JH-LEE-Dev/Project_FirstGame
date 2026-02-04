53b911d0ec704ee5b2757cb8976e1f941fe0e067
JH-LEE-Dev <140565804+JH-LEE-Dev@users.noreply.github.com>
2026-02-04 19:00:00 +0900

    추가 커밋.

--- Assets/Scripts/Application Layer/CardSystem/CardSystemController/CardSystemConrollerUsingData.cs
+++ Assets/Scripts/Application Layer/CardSystem/CardSystemController/CardSystemConrollerUsingData.cs
@@ -154,6 +154,7 @@ public enum CardSystemActionTimingType
     AfterCardUsingPhase,
 }
 
+//AfterAttack에서는 반드시 StatusEffect만이 존재해야 함. 게임 로직으로도.
 //BeforeAttack -> AfterAttack -> BeforeCardUsingPhase -> AfterCardUsingPhase -> Turn End -> Before Turn ... 
 
 public enum CardDataControlSystemActionType
