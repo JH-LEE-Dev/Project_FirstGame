f732f00a55cbeb560597b4a7ae0edfa443361398
VOID <void91112@gmail.com>
2026-02-05 10:43:35 +0900

    🔶 메인메뉴 변경

--- Assets/Scripts/Presentation Layer/UISystem/UIView/MainMenuUI/UIView_MainMenu.cs
+++ Assets/Scripts/Presentation Layer/UISystem/UIView/MainMenuUI/UIView_MainMenu.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using System;
+using Unity.VisualScripting;
 
 public class UIView_MainMenu : UIView
 {
@@ -13,6 +14,12 @@ public class UIView_MainMenu : UIView
 
     [Header("Buttons")]
     [SerializeField] private MenuButton startButton;
+    [SerializeField] private MenuButton creditButton;
+    [SerializeField] private MenuButton optionButton;
+    [SerializeField] private MenuButton exitButton;
+
+    [Header("Other UI's")]
+    [SerializeField] private WarningUI warningUI;
 
     protected override void Awake()
     {
@@ -21,7 +28,10 @@ public class UIView_MainMenu : UIView
         if (uiPrefab != null)
             Instantiate(uiPrefab, uiRoot);
 
-        startButton.OnCompleteAction(OnClickStart);
+        startButton?.OnCompleteAction(OnClickStart);
+        exitButton?.OnCompleteAction(OnClickExit);
+        creditButton?.OnCompleteAction(OnClickCredit);
+        optionButton?.OnCompleteAction(OnClickOption);
     }
 
     public override void OnDestroy()
@@ -52,5 +62,20 @@ public class UIView_MainMenu : UIView
     public void OnClickStart()
     {
         PlayButtonClickedEvent?.Invoke();
+    }
+
+    public void OnClickExit()
+    {
+        warningUI?.Play("아직 로직이 없어요");
+    }
+
+    public void OnClickCredit()
+    {
+        warningUI?.Play("아직 로직이 없어요");
+    }
+
+    public void OnClickOption()
+    {
+        warningUI?.Play("아직 로직이 없어요");
     }
 }
