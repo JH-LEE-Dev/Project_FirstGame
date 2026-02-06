9a67b5678f2aee9d5fb9cac48289fa8c9027ee0d
VOID <void91112@gmail.com>
2026-02-06 11:11:22 +0900

    🔶 EnemyHPBar 작업 중

--- Assets/Scripts/Presentation Layer/UISystem/UIView/UnitUI/UIView_Unit_Canvas.cs
+++ Assets/Scripts/Presentation Layer/UISystem/UIView/UnitUI/UIView_Unit_Canvas.cs
@@ -6,6 +6,8 @@ public class UIView_Unit_Canvas : UIView
     [Header("UI References")]
     [SerializeField] private Transform uiRoot;
 
+    [Header("Pooling System")]
+    [SerializeField] private ObjectPoolingSystem healthPool;
 
     protected override void Awake()
     {
@@ -30,5 +32,26 @@ public class UIView_Unit_Canvas : UIView
     protected override void OnHide()
     {
         base.OnHide();
+    }
+
+    private HPBar_Enemy GetHealthBar()
+    {
+        GameObject obj = healthPool.Pool.Get();
+        HPBar_Enemy bar = obj?.GetComponent<HPBar_Enemy>();
+        if (null == bar)
+            return null;
+
+        obj.SetActive(true);
+
+        return bar;
+    }
+
+    private void BindingEnemy(Enemy _target)
+    {
+        HPBar_Enemy hpBar = GetHealthBar();
+        if (null == hpBar)
+            return;
+
+        hpBar.Init(_target);
     }
 }
--- Assets/Scripts/Presentation Layer/UISystem/UI_Enemy/HPBar_Enemy.cs
+++ Assets/Scripts/Presentation Layer/UISystem/UI_Enemy/HPBar_Enemy.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 public class HPBar_Enemy : MonoBehaviour
 {
     [Header("Main Settings")]
-    //[SerializeField] private script visual; // 나중에 비주얼쪽 스크립트 물려 받으면 오프셋 받아 올 것임 
-    [SerializeField] private SpriteRenderer hpSlider;
+    private Enemy owner;
+    [SerializeField] private Slider hpSlider;
     [SerializeField] private TMP_Text hpText;
 
     private void Awake()
@@ -14,13 +14,33 @@ public class HPBar_Enemy : MonoBehaviour
 
     }
 
+    private void LateUpdate()
+    {
+        Update_Position();
+    }
+
+    public void Init(Enemy target)
+    {
+        owner = target;
+    }
+
     public void Update_HPBar(float ratio)
     {
-        
+        if (null == hpSlider)
+            return;
+
+        hpSlider.value = Mathf.Clamp01(ratio);
     }
 
     public void Update_HPText(float _currentValue)
     {
         hpText.text = Mathf.RoundToInt(_currentValue).ToString();
+    }
+
+    private void Update_Position()
+    {
+        // 오프셋을 빼와서 추가 연산도 해야 함
+
+
     }
 }
