38af5494845af3241f7969b5f008dedc45d4fc4c
JH-LEE-Dev <140565804+JH-LEE-Dev@users.noreply.github.com>
2026-02-12 14:44:50 +0900

    디버프 미적용 버그 수정.

--- Assets/Scripts/Application Layer/CardSystem/Card/CardDataInstance.cs
+++ Assets/Scripts/Application Layer/CardSystem/Card/CardDataInstance.cs
@@ -41,7 +41,6 @@ public class CardDataInstance : ICardDataInstanceProvider
     {
         this.cardData = cardData;
         ResetState();
-
         ReadyEffects();
     }
 
@@ -112,17 +111,12 @@ public class CardDataInstance : ICardDataInstanceProvider
 
         for (int i = 0; i < cardData.defaultElementTypes.Count; ++i)
         {
-            if (elementTypes.ContainsKey(cardData.defaultElementTypes[i].bulletElementType))
-            {
-                elementTypes[cardData.defaultElementTypes[i].bulletElementType] = new BulletElementData(cardData.defaultElementTypes[i].bulletElementType, cardData.defaultElementTypes[i].nestingCnt);
-            }
+            elementTypes[cardData.defaultElementTypes[i].bulletElementType] = new BulletElementData(cardData.defaultElementTypes[i].bulletElementType, cardData.defaultElementTypes[i].nestingCnt);
         }
+
         for (int i = 0; i < cardData.defaultdebuffTypes.Count; ++i)
         {
-            if (debuffTypes.ContainsKey(cardData.defaultdebuffTypes[i].debuffElementType))
-            {
-                debuffTypes[cardData.defaultdebuffTypes[i].debuffElementType] = new DebuffElementData(cardData.defaultdebuffTypes[i].debuffElementType, cardData.defaultdebuffTypes[i].turnCnt);
-            }
+            debuffTypes[cardData.defaultdebuffTypes[i].debuffElementType] = new DebuffElementData(cardData.defaultdebuffTypes[i].debuffElementType, cardData.defaultdebuffTypes[i].turnCnt);
         }
     }
 
